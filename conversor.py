9284d416e8fb4a48a64c5213d37ccfcf54fd29bf
NPSoto <nicolas.soto@usach.cl>
2021-05-28 10:14:24 -0400

    Conversión con menú sin funciones

--- conversor.py
+++ conversor.py
@@ -1,7 +1,45 @@
-pesos = input("¿Cuántos pesos chilenos tienes?: ")
-pesos = float(pesos)
-valor_dolar = 733
-dolares = pesos / valor_dolar
-dolares = round(dolares, 2)
-dolares = str(dolares)
-print("Tienes $" + dolares + " dolares")
+menu = """
+Bienvenido al conversor de monedas
+
+1 - Pesos chilenos
+2 - Pesos argentinos
+3 - Pesos mexicanos
+
+Elige una opción: """
+
+opcion = int(input(menu))
+
+if opcion == 1:
+    pesos = input("¿Cuántos pesos chilenos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 733
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 2:
+    pesos = input("¿Cuántos pesos argentinos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 65
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+elif opcion == 3:
+    pesos = input("¿Cuántos pesos mexicanos tienes?: ")
+    pesos = float(pesos)
+    valor_dolar = 24
+    dolares = pesos / valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " dolares")
+else:
+    print("Ingresa una opción válida por favor")
+
+# pesos = input("¿Cuántos pesos chilenos tienes?: ")
+# pesos = float(pesos)
+# valor_dolar = 733
+# dolares = pesos / valor_dolar
+# dolares = round(dolares, 2)
+# dolares = str(dolares)
+# print("Tienes $" + dolares + " dolares")
