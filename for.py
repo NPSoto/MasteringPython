a8ee4dcd50a14ce2a9e956b28f62a67088b9687d
NPSoto <nicolas.soto@usach.cl>
2021-05-28 12:03:23 -0400

    ciclo for en python usando range con 2 parametros

---
+++ for.py
@@ -0,0 +1,14 @@
+# contador = 1 
+# print(str(contador))
+# while contador < 1000:
+#     contador  += 1
+#     print (str(contador))
+
+# a = listrange(1000)
+# print(a)
+
+for contador in range(1, 1001):
+    print(contador)
+
+for i in range(10):
+    print(11 * i)
