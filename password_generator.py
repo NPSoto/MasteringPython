58542b87555d8610d2eda3a7f5c51b0c6107c72a
NPSoto <nicolas.soto@usach.cl>
2021-05-28 14:13:15 -0400

    Final Project, password generator using diccionaries, random import, join method

---
+++ password_generator.py
@@ -0,0 +1,27 @@
+import random
+
+def generate_password():
+    UPPERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'X', 'Y', 'Z']
+    LOWERS = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z']
+    NUMS = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+    CHARS = ['*', '+', '-', '/', '@', '_', '?', '!', '[', '{', '(', ')', '}', ']', ',', ';', '.', '>', '<', '~', '°', '^', '&', '$', '#', '"']
+    
+    pool_of_characters = UPPERS + LOWERS + NUMS + CHARS
+
+    password = []
+
+    for i in range(15):
+        random_selection = random.choice(pool_of_characters)
+        password.append(random_selection)
+
+    password = "".join(password)
+    return password
+
+def run():
+    password = generate_password()
+    print (' Your new password is: ' + password) 
+
+
+
+if __name__ == '__main__':
+    run()
